4267dd63db0a6f912ebc5fd38dccde173839d1a3
ktoto2007 <159888941+ktoto2007@users.noreply.github.com>
2025-03-10 16:32:22 +0300

    Delete task2 directory

--- task2/task2.js
+++
@@ -1,8 +0,0 @@
-let student = {
-    name: "Bob",
-    age: 22,
-    grades: [90, 85, 88]
-};
-const hasGrades = (user) => {
-    return Object.hasOwn(user, 'grades') ? console.log('Yes') : console.log('No')
-}
